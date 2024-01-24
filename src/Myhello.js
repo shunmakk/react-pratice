6780ae13083d5b8fda4915bf5e60e86e478d108a
shunmakk <shunmakajioka12@gmail.com>
2024-01-24 21:51:23 +0900

    propsの基本

---
+++ src/Myhello.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Myhello = (props) => {
+  return (
+    <div>こんにちは{props.myName}さん</div>
+  )
+}
+
+export default Myhello
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Myhello from './Myhello';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Myhello myName="お馬鹿" />
   </React.StrictMode>
 );
 
