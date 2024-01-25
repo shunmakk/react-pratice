b2991e53566c21dfaf49b04c9a42f96b921ed226
shunmakk <shunmakajioka12@gmail.com>
2024-01-25 17:28:23 +0900

    stateにコンソールログを追加しdeveloperツールで確認しやすくする

--- src/components/StateBasic.js
+++ src/components/StateBasic.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 const StateBasic = ({init}) => {
    
     const [count,setCount] = useState(init);
+    console.log(`count is ${count}.`);
 
     const handleClick = () => setCount(count + 1);
 
